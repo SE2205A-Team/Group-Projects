84bdcb1f606c41ef80e8d9225a081a51f2302f76
luxscious <ggerges019@gmail.com>
2020-09-29 15:15:44 -0400

    Task 2-1 Done

--- assignments/src/com/projects/doublelinkedlist/Main.java
+++ assignments/src/com/projects/doublelinkedlist/Main.java
@@ -1,10 +1,21 @@
 package com.projects.doublelinkedlist;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		DoubleLinkedList<Object> Objects = new DoubleLinkedList<>(); // Array of DoubleLinkedList Objects
+        int arbitraryNum = 5; // Arbitrary Number of Items, Doesn't have to be 5
+		  
+        Scanner keyboard = new Scanner(System.in);
+		  
+		 System.out.println("Please enter "+ arbitraryNum +" Objects");
+		  
+		 for(int i = 0; i< arbitraryNum; i++) { //Loops through objects in the array
+			 System.out.println("Object: 0"+(i+1)); //Display of which object in the array it's on
+		 
+		 }
 	}
 
 }
