6d00f322206eff5927ac4564bfa583b28d05285f
doccmod <57057971+doccmod@users.noreply.github.com>
2020-09-30 19:25:30 -0400

    Full Project
    All tasks are done, take a look if theres anything that needs changing/anything I missed

--- assignments/src/com/projects/doublelinkedlist/DoubleLinkedList.java
+++ assignments/src/com/projects/doublelinkedlist/DoubleLinkedList.java
@@ -1,5 +1,161 @@
 package com.projects.doublelinkedlist;
 
-public class DoubleLinkedList {
+// Doubly Linked List class
+public class DoubleLinkedList<E> {
 
+	// nested node class
+	private static class Node<E> {
+		
+		// element
+		private E element;
+		
+		// reference to the previous node
+		private Node<E> prev;
+		
+		// reference to the next node
+		private Node<E> next;
+		
+		// parameters for the class
+		public Node(E e, Node<E> p, Node<E> n) {
+			element = e;
+			prev = p;
+			next = n;
+		}
+		
+		// nodes and functions
+		public E getElement() {return element; }
+		public Node<E> getPrev() { return prev; }
+		public Node<E> getNext() {return next; }
+		public void setPrev(Node<E> p) {prev = p; }
+		public void setNext(Node<E> n) {next = n; }
+	}
+	
+	// header and trailer nodes
+	private Node<E> header;
+	private Node<E> trailer;
+	
+	// list size
+	private int size = 0;
+	
+	// constructor
+	public DoubleLinkedList() {
+		
+		// placing the header at the beginning and the trailer at the end
+		header = new Node<>(null, null, null);
+		trailer = new Node<>(null, header, null);
+		
+		// connecting the header and the trailer
+		header.setNext(trailer);
+	}
+	
+	// size method
+	public int size() { return size; }
+	
+	// sets size to 0 if the list is empty
+	public boolean isEmpty() { return size == 0; }
+	
+	public E first() {
+		
+		// if the list is empty, return null
+		if(isEmpty()) return null;
+		
+		// return the element after the header node
+		return header.getNext().getElement();
+	}
+	
+	public E last() {
+		
+		// if the list is empty, return null
+		if(isEmpty()) return null;
+		
+		// return the element before the trailer node
+		return trailer.getPrev().getElement();
+	}
+	
+	// method for adding an element at the beginning
+	public void addFirst(E e) {
+		addBetween(e, header, header.getNext());
+	}
+	
+	// method for adding an element at the end
+	public void addLast(E e) {
+		addBetween(e, trailer.getPrev(), trailer);
+	}
+	
+	// method for removing the first node in the list
+	public E removeFirst() {
+		if(isEmpty()) return null;
+		return remove(header.getNext());
+	}
+	
+	// method for removing the last node in the list
+	public E removeLast() {
+		if(isEmpty()) return null;
+		return remove(trailer.getPrev());
+	}
+	
+	// method for adding a new node in between
+	private void addBetween(E e, Node<E> predecessor, Node<E> successor) {
+		
+		// new node called newest, put in between its predecessor and successor
+		Node<E> newest = new Node<>(e, predecessor, successor);
+		predecessor.setNext(newest);
+		successor.setPrev(newest);
+		
+		// increases the size of the list
+		size++;
+	}
+	// method for removing a node
+	private E remove(Node<E> node) {
+		
+		// removes the node and connects the successor and predecessor
+		Node<E> predecessor = node.getPrev();
+		Node<E> successor = node.getNext();
+		predecessor.setNext(successor);
+		successor.setPrev(predecessor);
+		
+		// decreases the size of the list
+		size--;
+		
+		// returns the node that was deleted
+		return node.getElement();
+	}
+	
+	// displays the list in order
+	public void display() {
+		
+		// starts with the first node which contains an element
+        Node<E> currentNode = header.getNext();
+    	System.out.println("\nEntity list in regular order: ");
+        for(int i = 0; i < size; i++) {
+        	
+        	// prints that element
+            System.out.print(currentNode.getElement());
+            if(i < (size - 1)) {
+				System.out.print(", ");
+			}
+            
+            // gets the next node in the list
+            currentNode = currentNode.getNext();
+        }
+	}
+	
+	// displays the list in reverse order
+	public void displayReverse() {
+		
+		// starts with the last node that contains an element
+		Node<E> currentNode = trailer.getPrev();
+		System.out.println("\n\nEntity list in reverse order: ");
+		for(int i = 0; i < size; i++) {
+			
+			// prints that element
+			System.out.print(currentNode.getElement());
+			if(i < (size - 1)) {
+				System.out.print(", ");
+			}
+			
+			// gets the previous node in the list
+			currentNode = currentNode.getPrev();
+		}
+	}
 }
--- assignments/src/com/projects/doublelinkedlist/Main.java
+++ assignments/src/com/projects/doublelinkedlist/Main.java
@@ -1,21 +1,88 @@
 package com.projects.doublelinkedlist;
 
+// importing libraries
 import java.util.Scanner;
 
+// main class
 public class Main {
 
+	// main method
 	public static void main(String[] args) {
-		DoubleLinkedList<Object> Objects = new DoubleLinkedList<>(); // Array of DoubleLinkedList Objects
-        int arbitraryNum = 5; // Arbitrary Number of Items, Doesn't have to be 5
-		  
+		
+		// creating the scanner object
         Scanner keyboard = new Scanner(System.in);
-		  
-		 System.out.println("Please enter "+ arbitraryNum +" Objects");
-		  
-		 for(int i = 0; i< arbitraryNum; i++) { //Loops through objects in the array
-			 System.out.println("Object: 0"+(i+1)); //Display of which object in the array it's on
+        
+		// creating a new doubly linked list object
+		DoubleLinkedList<String> list = new DoubleLinkedList<>();
+		
+		// initializing the list size and entry variables
+        int arbitraryNum;
+        String entry;
 		 
-		 }
+        // asking the user for how many entities they want
+		System.out.print("Please enter the number of entities wanted: ");
+		
+		// storing the amount entered
+		arbitraryNum = keyboard.nextInt();
+		System.out.println("");
+		
+		// getting the entities from the user
+		for(int i = 0; i < arbitraryNum; i++) {
+			System.out.print("Enter entry " + (i + 1) + ": ");
+			entry = keyboard.next();
+							
+			// adding the entities to the list
+			list.addLast(entry);
+		}
+		
+		// displaying the list in regular order
+		list.display();
+		
+		// displaying the list in reverse order
+		list.displayReverse();
+		
+		System.out.println("\n\nRemoving all entities and printing in regular order...");
+		
+		// print and delete method
+		for(int i = 0; i < arbitraryNum; i ++) {
+			
+			// prints the first entity in the list
+			System.out.print(list.first());
+			if(i < (arbitraryNum - 1)) {
+				System.out.print(", ");
+			}
+			
+			// removes the first entity from the list
+			list.removeFirst();
+		}
+		
+		System.out.println("\n\nPlease repopulate the list...\n");
+		
+		// getting the entities from the user
+		for(int i = 0; i < arbitraryNum; i++) {
+			System.out.print("Enter entry " + (i + 1) + ": ");
+			entry = keyboard.next();
+					
+			// adding the entities to the list
+			list.addLast(entry);
+		}
+		
+		System.out.println("\nRemoving all entities and printing in reverse order...");
+		
+		// reverse delete and print method
+		for(int i = 0; i < arbitraryNum; i++) {
+			
+			// prints the last entity in the list
+			System.out.print(list.last());
+			if(i < (arbitraryNum - 1)) {
+				System.out.print(", ");
+			}
+			
+			// removes the last entity from the list
+			list.removeLast();
+		}
+		
+		// closing the scanner object
+		keyboard.close();
 	}
-
 }
